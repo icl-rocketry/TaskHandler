622ae964cf3c68295503ef9624b45be15df715d8
Max Hallgarten La Casta <34109153+maxhlc@users.noreply.github.com>
2024-03-03 20:23:26 +0000

    Formatting updates

--- src/lib/TasksList.svelte
+++ src/lib/TasksList.svelte
@@ -1,67 +1,82 @@
 <script>
-  export let tasks
-  import NiceButton from "./NiceButton.svelte"
-    import TaskButton from "./TaskButton.svelte";
-  import ToggleButton from "./ToggleButton.svelte"
+  // Export tasks list
+  export let tasks;
 
-  import { createEventDispatcher } from "svelte"
-  const dispatch = createEventDispatcher()
+  // Standard imports
+  import { createEventDispatcher } from "svelte";
+
+  // Internal imports
+  import NiceButton from "./NiceButton.svelte";
+  import TaskButton from "./TaskButton.svelte";
+  import ToggleButton from "./ToggleButton.svelte";
+
+  // Create event dispatcher
+  const dispatch = createEventDispatcher();
 </script>
 
-<div class='taskListDiv'>
+<div class="taskListDiv">
   <h3>Tasks</h3>
-  <table class='tasktable'>
+  <table class="tasktable">
     {#each tasks as task}
       <tr>
         <td>
-          <TaskButton on:click={() => dispatch('selectTask', {'task_name': task.task_name})} text={task.task_name} connected={task.connected}/>
+          <TaskButton
+            on:click={() =>
+              dispatch("selectTask", { task_name: task.task_name })}
+            text={task.task_name}
+            connected={task.connected}
+          />
         </td>
         <td>
-          <ToggleButton on:click={() => dispatch('toggleTask', {'task_name': task.task_name})} toggle={task.running}/>
+          <ToggleButton
+            on:click={() =>
+              dispatch("toggleTask", { task_name: task.task_name })}
+            toggle={task.running}
+          />
         </td>
         <td>
-          <div class='stats'>
+          <div class="stats">
             <div class="labelwidth">
-              tx 
-              <br>
-              rx 
+              tx
+              <br />
+              rx
             </div>
             <div class="counterwidth">
               {task.txCounter}
-              <br>
+              <br />
               {task.rxCounter}
             </div>
           </div>
         </td>
       </tr>
     {/each}
-    </table>
-  
+  </table>
 </div>
+
 <style>
-  .tasktable{
+  .tasktable {
     list-style-type: none;
     padding: 0;
     text-align: center;
     border: 1px solid #ccc;
     width: 100%;
   }
-  .taskListDiv{
+  .taskListDiv {
     border: 1px solid #ccc;
     height: 40vh;
     overflow: auto;
   }
-  .stats{
+  .stats {
     font-size: 14px;
     background-color: #1a1a1a;
     display: flex;
     justify-content: flex-start;
   }
-  .labelwidth{
+  .labelwidth {
     width: 10px;
     padding-right: 10px;
   }
-  .counterwidth{
+  .counterwidth {
     width: 90px;
     text-align: left;
   }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,50 @@
 <script>
+  // Standard imports
   import { onMount, onDestroy } from "svelte";
+
+  // Third-party imports
   import io from "socket.io-client";
 
-  import iclrLogo from "$lib/assets/ICLR-LOGO.png";
-  import fakeJson from "$lib/assets/fakeJson.json";
-
+  // Internal imports
+  import ConsoleBox from "$lib/ConsoleBox.svelte";
+  import JSONEditor from "$lib/JSONEditor.svelte";
   import NiceButton from "$lib/NiceButton.svelte";
   import TasksList from "$lib/TasksList.svelte";
-  import JSONEditor from "$lib/JSONEditor.svelte";
-  import ConsoleBox from "$lib/ConsoleBox.svelte";
 
+  // Internal asset imports
+  import fakeJson from "$lib/assets/fakeJson.json";
+  import iclrLogo from "$lib/assets/ICLR-LOGO.png";
+
+  // Declare list for tasks
   let tasks = [];
-  //tasks = fakeJson
+
+  // Declare string for selected task contents
   let taskCopy = "Click on a task to edit it";
 
-  let poll_interval;
+  // Declare socket variable
   let socket;
+
+  // Declare polling variables
+  let poll_interval;
   let poll_time = 0;
+
+  // Declare string for log messages
   let log = "";
 
   function checkDeltaPoll() {
+    // Check if sufficient time has passed since the last poll (100 ms)
     if (Date.now() - poll_time > 100) {
+      // Update last poll time
       poll_time = Date.now();
+
+      // Emit message to request task update
       socket.emit("getRunningTasks");
     }
   }
 
   onMount(() => {
+    // Connect to backend Socket.IO
+    // TODO: update URL
     socket = io(
       "http://" +
         location.hostname +
@@ -34,63 +52,100 @@
         location.port +
         "/data_request_handler",
     );
-    //dev only
-    // socket = io('http://localhost:1337/data_request_handler')
 
+    // Set connection callback
     socket.on("connect", () => {
+      // Start polling task (repeats every 1 ms)
       poll_interval = setInterval(checkDeltaPoll, 1);
     });
+
+    // Set running tasks callback
     socket.on("runningTasks", (data) => {
+      // Update tasks
       tasks = data;
+
+      // Update last poll time
       poll_time = Date.now();
     });
   });
 
   onDestroy(() => {
+    // Stop polling task
     clearInterval(poll_interval);
   });
 
   function onSelectTask(event) {
+    // Find index of task by name
     var taskIndex = tasks.findIndex(function (task) {
       return task.task_name == event.detail.task_name;
     });
+
+    // Update string for selected task contents
     taskCopy = JSON.stringify(tasks[taskIndex], null, 2);
   }
 
   function onToggleTask(event) {
+    // Find index of task by name
     var taskIndex = tasks.findIndex(function (task) {
       return task.task_name == event.detail.task_name;
     });
+
+    // Extract task from task list
     var taskToToggle = tasks[taskIndex];
+
+    // Toggle run flag
     taskToToggle.running = !taskToToggle.running;
+
+    // Emit message to update task
     socket.emit("newTaskConfig", taskToToggle);
   }
 
   function onRefreshTasks() {
+    // Emit message to request task update
     socket.emit("getRunningTasks");
+
+    // Log task update request
     log = log + timeString() + ` Manually sending getRunningTasks \n`;
   }
 
   function onNewTask() {
-    var newTask = { ...fakeJson[0] }; //Creates a copy instead of referencing fakeJson
+    // Create a copy of the default task JSON
+    var newTask = { ...fakeJson[0] };
+
+    // Update task name
     newTask.task_name = "newtask_" + String(tasks.length);
+
+    // Update string for selected task contents
     taskCopy = JSON.stringify(newTask, null, 2);
   }
 
   function onClearTasks() {
+    // Emit message to clear tasks
     socket.emit("clearTasks");
+
+    // Log clearing of tasks
     log = log + timeString() + ` Cleared all tasks \n`;
   }
 
   function onSaveTask() {
+    // Parse selected task contents
     var taskObject = tryParse(taskCopy);
+
+    // Emit message to save task contents
     socket.emit("saveHandlerConfig", taskObject);
+
+    // Log task save
     log = log + timeString() + ` Saving ${taskObject.task_name} \n`;
   }
 
   function onUpdateTask() {
+    // Parse selected task contents
     var taskObject = tryParse(taskCopy);
+
+    // Emit message to update task configuration
     socket.emit("newTaskConfig", taskObject);
+
+    // Log task update
     log =
       log +
       timeString() +
@@ -98,27 +153,40 @@
   }
 
   function onDeleteTask() {
+    // Parse selected task contents
     var taskObject = tryParse(taskCopy);
+
+    // Emit message to delete task configuration
     socket.emit("deleteTaskConfig", taskObject);
+
+    // Log task deletion
     log = log + timeString() + ` Deleting ${taskObject.task_name} \n`;
+
+    // Reset selected task contents
     taskCopy = "Click on a task to edit it";
   }
 
   function tryParse(data) {
     try {
+      // Return parsed JSON
       return JSON.parse(data);
     } catch (error) {
+      // Stringify error message
       const errorString = error.toString();
 
+      // Log error message
       log = log + timeString() + ` ${errorString} \n`;
     }
   }
 
   function timeString() {
+    // Extract current time
     const now = new Date();
     const hour = now.getHours();
     const minute = now.getMinutes();
     const second = now.getSeconds();
+
+    // Return current time string
     return `[${hour}:${minute}:${second}]`;
   }
 
@@ -128,7 +196,7 @@
       // Start task
       task.running = true;
 
-      // Emit update
+      // Emit message to update task run state
       socket.emit("newTaskConfig", task);
     }
   }
@@ -139,7 +207,7 @@
       // Stop task
       task.running = false;
 
-      // Emit update
+      // Emit message to update task run state
       socket.emit("newTaskConfig", task);
     }
   }
